298363a975eda85faf8806bf38efb77ff10dca88
jichou520 <64917863+jichou520@users.noreply.github.com>
2022-11-24 22:58:15 +0800

    Create cubox.js

---
+++ cubox.js
@@ -0,0 +1,37 @@
+/******************************
+
+脚本功能：cubox-收藏阅读+解锁VIP
+
+
+
+*******************************
+
+[rewrite_local]
+
+^https:\/\/cubox\.pro\/c\/api\/userInfo url script-response-body https://raw.githubusercontent.com/jichou520/happy/main/Cubox.js
+
+[mitm] 
+
+hostname = cubox.pro
+
+*******************************/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/userInfo';
+
+
+if (url.indexOf(vip) != -1) {
+    obj.data.level = 1;
+    obj.data.expireTime = "2122-09-12T23:50:23+08:00";
+    obj.data.isExpire = false;
+    obj.data.active = true;
+    obj.data.payTime = 1660006006;
+
+	body = JSON.stringify(obj);
+}
+
+
+$done({body});
